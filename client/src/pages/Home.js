8b0a42fb0426213b02e2672fe72d3b936052b7fd
Preston Fassbender <fassbenderp0551@gmail.com>
2022-04-21 17:53:02 -0500

    it don't work but whatever

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
-import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
 
-
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 
 const Home = () => {
-    // use useQuery hook to make query request
     const { loading, data } = useQuery(QUERY_THOUGHTS);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
     const thoughts = data?.thoughts || [];
-    console.log(thoughts);
 
     const loggedIn = Auth.loggedIn();
-
-    // use object destructuring to extract `data` from the `useQuery` Hook's response and rename it `userData` to be more descriptive
-    const { data: userData } = useQuery(QUERY_ME_BASIC);
 
     return (
         <main>
@@ -25,7 +20,10 @@ const Home = () => {
                     {loading ? (
                         <div>Loading...</div>
                     ) : (
-                        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+                        <ThoughtList
+                            thoughts={thoughts}
+                            title="Some Feed for Thought(s)..."
+                        />
                     )}
                 </div>
                 {loggedIn && userData ? (
